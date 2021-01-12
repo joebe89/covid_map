4a4f0c5931e9258cabf9f8c69df4fcc3b0f21862
Mathew Powell <mpowell90gds@bitbucket.org>
2021-01-12 21:54:52 +0000

    Removed unrequired dependencies

--- web/next.config.js
+++ web/next.config.js
@@ -1,13 +1,15 @@
-const withSass = require('@zeit/next-sass');
+const path = require('path');
 
 module.exports = {
-	...withSass(),
-	webpack: config => {
-		// Fixes npm packages that depend on `fs` module
-		config.node = {
-			fs: 'empty',
-		};
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+  webpack: config => {
+    // Fixes npm packages that depend on `fs` module
+    config.node = {
+      fs: 'empty'
+    }
 
-		return config;
-	},
-};
+    return config
+  }
+}
